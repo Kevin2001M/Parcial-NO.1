41b42f27e124e7de051b5097ec3b3d2f6a5e8b45
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2021-09-09 12:32:26 -0600

    Partes

---
+++ KevinMauricioAlvarengaHenriquez/KevinMauricioAlvarengaHenriquez/Models/CajeroViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KevinMauricioAlvarengaHenriquez.Models
+{
+    public class CajeroViewModel
+    {
+        public CajeroViewModel(int monto)
+        {
+            this.monto = monto;
+        }
+
+        public int monto { get; set; }
+    }
+
+}

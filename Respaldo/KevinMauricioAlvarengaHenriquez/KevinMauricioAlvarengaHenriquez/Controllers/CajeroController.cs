9062bc9644dbf3626e2ff485e7afe7783cc11b9d
Kevin2001M <78760245+Kevin2001M@users.noreply.github.com>
2021-09-09 12:35:33 -0600

    Respaldo

---
+++ Respaldo/KevinMauricioAlvarengaHenriquez/KevinMauricioAlvarengaHenriquez/Controllers/CajeroController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KevinMauricioAlvarengaHenriquez.Controllers
+{
+    public class CajeroController : Controller
+    {
+        // GET: Cajero
+
+        [HttpPost]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Error()
+        {
+            return View();
+        }
+        public ActionResult Correcto()
+        {
+            return View();
+        }
+    }
+}
